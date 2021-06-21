e2b2b36c7bb6becf410d06871e944e9e05ec4a12
ipulsamudin <ipulsamudin@gmail.com>
2021-06-21 11:06:44 +0700

    membuat tampilan login

---
+++ application/views/Admin/login/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.2/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/dist/css/login.css">
+</head>
+<body class="main-bg">
+    <div class="login-container text-c animated flipInX">
+        <div>
+            <h1 class="logo-badge text-whitesmoke"><span class="fa fa-user-circle"></span></h1>
+        </div>
+            <h3 class="text-whitesmoke">Pingfest 2021</h3>
+            <p class="text-whitesmoke">Sign In</p>
+        <div class="container-content">
+            <form class="margin-t">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Username" required="">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="*****" required="">
+                </div>
+                <button type="submit" class="form-button button-l margin-b">Sign In</button>
+
+                <!-- <a class="text-darkyellow" href="#"><small>Forgot your password?</small></a>  -->
+            </form>
+            <p class="margin-t text-whitesmoke"><small> Pingfest &copy; 2021</small> </p>
+        </div>
+    </div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</body>
+</html>
--- application/views/Admin/templates/header.php
+++ application/views/Admin/templates/header.php
@@ -1,10 +1,4 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Admin Ping | Dashboard</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
