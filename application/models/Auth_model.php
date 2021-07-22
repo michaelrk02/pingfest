0cf7dd3f6e6cabfe9743c2b68164ffd0841bfe76
alwizzz <alwiza21@gmail.com>
2021-07-22 17:52:09 +0700

    tambah model Auth_model.php

---
+++ application/models/Auth_model.php
@@ -0,0 +1,51 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth_model extends CI_model 
+{
+    public function addUserData()
+    {
+        $data = [
+            'user_id' => $this->input->post('uname'),
+            'password' => password_hash($this->input->post('password'), PASSWORD_BCRYPT),
+            'name' => $this->input->post('name'),
+            'email' => $this->input->post('email'),
+            'phone' => $this->input->post('phone'),
+            'hash' => md5( $this->input->ip_address())
+        ];
+
+        $this->db->insert('users', $data); 
+    }
+
+    public function login(){
+        $uname = $this->input->post('uname', true);
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('users', ['user_id' => $uname])->row_array();
+        if( $user != NULL ){
+            //Username bener
+            if(password_verify($password, $user['password'])){
+                //password bener
+                $session_data = [
+                    'user_id' => $user['user_id']
+                ];
+                $this->session->set_userdata($session_data);
+                redirect(site_url('profile/index/'));
+            } else {
+                $session_data = [
+                    'auth_msg' => '<div class="alert alert-danger" role="alert">Username atau Password salah!</div>'
+                ];
+                $this->session->set_userdata($session_data);
+                redirect(base_url('auth/index'));
+            }
+        } else {
+            $session_data = [
+                'auth_msg' => '<div class="alert alert-danger" role="alert">Username atau Password salah!</div>'
+            ];
+            $this->session->set_userdata($session_data);
+            redirect(base_url('auth/index'));
+        }
+    }
+
+}
+
+?>
