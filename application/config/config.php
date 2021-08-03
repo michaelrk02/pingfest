57fbc3f3876901d081911988ae426e9918f96344
Michael R. Krisnadhi <michaelkrisnadhi@gmail.com>
2021-08-03 22:11:17 +0700

    add rewrite config

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = PF_URL;
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = PF_REWRITE_ENABLED ? '' : 'index.php';
 
 /*
 |--------------------------------------------------------------------------
