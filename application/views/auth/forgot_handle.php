7d6ca256f0fdfe9a29510f6eb4d47fecf74641ed
Drithh <adrielalfeus@gmail.com>
2021-08-05 22:44:57 +0700

    forgot handle, message style

--- application/views/auth/forgot_handle.php
+++ application/views/auth/forgot_handle.php
@@ -1,14 +1,15 @@
+<link rel="stylesheet" href="<?php echo base_url('public/pingfest/css/auth.min.css'); ?>" />
 
     <div class="container">
 
-<div class="card o-hidden border-0 shadow-lg my-5 col-lg-7 mx-auto">
+<div class="card o-hidden my-5 col-lg-7 mx-auto">
     <div class="card-body p-0">
         <!-- Nested Row within Card Body -->
         <div class="row">
             <div class="col-lg">
                 <div class="p-5">
                     <div class="text-center">
-                        <h1 class="h4 text-gray-900 mb-4">Ubah Password</h1>
+                        <h1 class="title h4 text-gray-900 mb-4">Ubah Password</h1>
                     </div>
                     <?php if( !empty($this->session->userdata('forgothandle_msg')) ){
                         echo $this->session->userdata('forgothandle_msg');
--- application/views/auth/message.php
+++ application/views/auth/message.php
@@ -1,14 +1,15 @@
+<link rel="stylesheet" href="<?php echo base_url('public/pingfest/css/auth.min.css'); ?>" />
 
     <div class="container">
 
-<div class="card o-hidden border-0 shadow-lg my-5 col-lg-7 mx-auto">
+<div class="card o-hidden my-5 col-lg-7 mx-auto">
     <div class="card-body p-0">
         <!-- Nested Row within Card Body -->
         <div class="row">
             <div class="col-lg">
                 <div class="p-5">
                     <div class="text-center align-middle alert alert-<?= $msg_type; ?>" role="alert">
-                        <h1 class="h4 text-gray-900 "><?= $msg_content; ?></h1>
+                        <h1 class="bree h4 text-gray-900 "><?= $msg_content; ?></h1>
                     </div>
                     <?php if( $isAddingLink ): ?>
                         <div class="text-center">
