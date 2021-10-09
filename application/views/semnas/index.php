86638084d35bb402916cf23d09bad50e2a2a4f5f
Drithh <adrielalfeus@gmail.com>
2021-10-09 18:27:57 +0700

    add sponsor, update foro pemb

--- application/views/semnas/index.php
+++ application/views/semnas/index.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="<?php echo base_url('public/pingfest/css/semnas.min.css'); ?>" />
+<link rel="stylesheet" href="<?php echo base_url('public/pingfest/css/sponsorlogo.min.css'); ?>" />
 
 
 
@@ -56,11 +57,47 @@
     <div class="pembicara-sem">
         <h1 class="title">Pembicara</h1>
         <div class="pembicara-wrapper">
-            <img src="<?php echo base_url('public/pingfest/img/sandhika.png'); ?>" alt="sandhika">
-            <img src="<?php echo base_url('public/pingfest/img/kevin.png'); ?>" alt="kevin">
+            <div class="pembicara">
+                <img src="<?php echo base_url('public/pingfest/img/sandhika.png'); ?>" alt="sandhika">
+                <h2>Sandhika Galih</h2>
+                <p>Content Creator</p>
+                <p>Online Construtor</p>
+            </div>
+            <div class="pembicara">
+                <img src="<?php echo base_url('public/pingfest/img/kevin.png'); ?>" alt="kevin">
+                <h2>Kevin Anggara</h2>
+                <p>Penulis - Blogger</p>
+                <p>Content Creator</p>
+            </div>
         </div>
     </div>
-    <div class="sponsor">
+    <div class="item sponsor">
+        <p>Our Sponsors</p>
+        <div class="sponsor-container">
+            <div class="size-section">
+                <a class="sponsor-logo sponsor-big" href="https://barito.tech/" target="_blank"><img
+                        class="sponsor-image" src="<?php echo base_url('public/pingfest/img/sponsor/bit.png'); ?>"
+                        alt=""></a>
+                <a class="sponsor-logo sponsor-big" href="https://upscale.co.id/" target="_blank"><img
+                        class="sponsor-image" src="<?php echo base_url('public/pingfest/img/sponsor/upscale.png'); ?>"
+                        alt=""></a>
+            </div>
+            <div class="size-section">
+                <a class="sponsor-logo sponsor-small" href="https://byu.id/" target="_blank"><img class="sponsor-image"
+                        src="<?php echo base_url('public/pingfest/img/sponsor/byu.png'); ?>" alt=""></a>
+                <a class="sponsor-logo sponsor-small" href=" https://dewaweb.com" target="_blank"><img
+                        class="sponsor-image" src="<?php echo base_url('public/pingfest/img/sponsor/dewaweb.png'); ?>"
+                        alt=""></a>
+                <a class="sponsor-logo sponsor-small" href="https://pahamify.com/" target="_blank"><img
+                        class="sponsor-image" src="<?php echo base_url('public/pingfest/img/sponsor/pahamify.png'); ?>"
+                        alt=""></a>
+                <a class="sponsor-logo sponsor-small" href="https://www.instagram.com/whoknowswhat.id/"
+                    target="_blank"><img class="sponsor-image"
+                        src="<?php echo base_url('public/pingfest/img/sponsor/wkw.png'); ?>" alt=""></a>
+
+            </div>
+
+        </div>
     </div>
 
 </div>
