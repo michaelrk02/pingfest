447d018af5aa0171786b4863eabcc72044e6e6b2
Drithh <adrielalfeus@gmail.com>
2021-10-09 14:42:26 +0700

    add fyi desc

--- application/views/semnas/index.php
+++ application/views/semnas/index.php
@@ -4,83 +4,42 @@
 
 <div class="headline">
     <div class="item-headline">
-        <h1>Perluas wawasanmu di</h1>
-        <h1 class="auto-scroll">Seminar Nasional</h1>
-        <div class="idea-container">
+        <h1>Webinar Nasional</h1>
+        <div class="auto-scroll idea-container">
             <div class="hay-text">Find</div>
             &#160;
             <div class="hay-text">Your</div>
             &#160;
             <div class="hay-text idea">Idea</div>
         </div>
+        <h1>Powered By Upscale & Bit</h1>
     </div>
 </div>
-<div class=container-sem>
+<div class="container-sem">
     <div class="deskripsi-sem">
-        <!-- <h2>Coming Soon!</h2> -->
-        <!-- <p>Acaranya masih dalam tahap perancangan, tapi apa salahnya kalau daftar sekarang
-            :))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))</p> -->
+        <div class="deskripsi-main">
+            <h2>Apa itu FYI?</h2>
+            <p>Find Your Idea merupakan acara puncak dari P!NGFEST yang dikemas dalam bentuk webinar.</p>
+            <p> Find Your Idea bertujuan untuk meingkatkan percaya diri pendengar untuk menemukan ide dan mengeluarkan
+                karya
+                yang dimiliki yang nantinya akan berguna untuk diri sendiri dan orang lain.</p>
+            <div class="daftar-sem button">
+                <a href="<?php echo site_url('profile/index').'?tab=e_semnas'; ?>" class="btn daftar-block">
+                    <span class="daftar-text">Daftar Sekarang</span>
+                </a>
+            </div>
+        </div>
+        <div class="deskripsi-text"></div>
         <div class="daftar-sem button">
-            <a href="<?php echo site_url('profile/index').'?tab=e_semnas'; ?>" class="btn daftar-block">
-                <span class="daftar-text">Daftar Sekarang</span>
-            </a>
+
         </div>
     </div>
+    <div class="logo-sem">
+        <img src="<?php echo base_url('public/pingfest/img/logosemnas.svg'); ?>" />
 
-    <!-- <div class="makan">
-
-        <div data-2400="opacity: 0;top: 50%;" data-2600="opacity: 1.5; " data-3000="" data-3200="opacity: 0; top: 70%;">
-            Why you keep scrolling?</div>
-
-        <div data-3400="opacity: 0;top: 0%;" data-3600="opacity: 1.5; top: 50%;" data-4000="margin-left: 0px;"
-            data-4200="opacity: 0; margin-left: -200px;">Hey!</div>
-
-
-        <div data-4400="opacity: 0;" data-4600="opacity: 1.5;" data-5200="" ; data-5400="opacity: 0;">Psst!</div>
-
-
-        <div data-5600="opacity: 0;top: 0%;" data-5800="opacity: 1.5; top: 50%;" data-6200="margin-left: 0px;"
-            data-6400="opacity: 0; margin-left: -200px;">Let me tell you something.</div>
-
-        <div data-6600="opacity: 0; " data-6800="opacity: 1; margin-top: 0px;" data-7000=""
-            data-8000="margin-top: -50px;" data-8800="" ; data-9200="opacity: 0;">When i was 5 years old.</div>
-
-        <div data-8000="opacity: 0;" data-8200="opacity: 1.5;" data-8800="" ; data-9200="opacity: 0;">I was five.</div>
-
-        <div data-9600="opacity: 0;top: 0%;" data-9800="opacity: 1.5; top: 50%;" data-10000="margin-left: 0px;"
-            data-12000="opacity: 0; margin-left: -200px;">How old were you?</div>
-
-        <div data-12400="opacity: 0;" data-12600="opacity: 1.5;" data-13200="" ; data-13400="margin-top: -50px;"
-            data-17800="" ; data-18000="opacity: 0;">If you think
-            about it,</div>
-
-        <div data-13600="opacity: 0;" data-13800="opacity: 1.5;" data-17800="" ; data-18000="opacity: 0;">2 + 3 is not
-            5.
-        </div>
-
-
-        <div data-15400="margin-left:0; margin-top: 50px; opacity: 0;" data-15600="opacity: 1.5;" data-16600=""
-            data-16800="margin-left:-35px;" data-17800="" ; data-18000="opacity: 0;">And
-        </div>
-
-        <div data-16800="left:13px; margin-top: 50px; opacity: 0;" data-17000="opacity: 1.5;" data-17800="" ;
-            data-18000="opacity: 0;">it
-        </div>
-
-        <div data-17000="left:48px; margin-top: 50px; opacity: 0;" data-17200="opacity: 1.5;" data-17800="" ;
-            data-18000="opacity: 0;">is.
-        </div>
-
-        <div data-17400="margin-top: 100px; opacity: 0; font-size: 16px;" data-17600="opacity: 1.5;" data-17800="" ;
-            data-18000="opacity: 0;">--random guy on internet--
-        </div>
-
-
-
-
-
-
-    </div> -->
+    </div>
+    <div class="informasi-sem"></div>
+    <div class="pembicara-sem"></div>
     <div class="sponsor">
     </div>
 
@@ -88,5 +47,4 @@
 
 
 
-<script src="<?php echo base_url('public/pingfest/js/skrollr.min.js'); ?>"></script>
 <script src="<?php echo base_url('public/pingfest/js/semnas.min.js'); ?>"></script>
