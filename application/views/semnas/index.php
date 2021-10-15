d8078d5af189923bae9e6ef34276b241906470fb
Drithh <adrielalfeus@gmail.com>
2021-10-15 13:15:17 +0700

    add benefit, update pembicara

--- application/views/semnas/index.php
+++ application/views/semnas/index.php
@@ -16,144 +16,160 @@
         <h1>Powered By Upscale & Bit</h1>
     </div>
 </div>
-<div class="container-sem">
-    <div class="deskripsi-sem">
-        <div class="deskripsi-main">
-            <h2>Apa itu FYI?</h2>
-            <p>Find Your Idea merupakan acara puncak dari P!NGFEST yang dikemas dalam bentuk webinar.</p>
-            <p> Find Your Idea bertujuan untuk meingkatkan percaya diri pendengar untuk menemukan ide dan mengeluarkan
-                karya
-                yang dimiliki yang nantinya akan berguna untuk diri sendiri dan orang lain.</p>
-            <div class="daftar-sem button">
-                <a href="<?php echo site_url('profile/index').'?tab=e_semnas'; ?>" class="btn daftar-block">
-                    <span class="daftar-text">Daftar Sekarang</span>
-                </a>
+<div class="container-wrapper">
+    <div class="container-sem">
+        <div class="deskripsi-sem">
+            <div class="deskripsi-main">
+                <h2>Apa itu FYI?</h2>
+                <p>Find Your Idea merupakan acara puncak dari P!NGFEST yang dikemas dalam bentuk webinar.</p>
+                <p> Find Your Idea bertujuan untuk meingkatkan percaya diri pendengar untuk menemukan ide dan
+                    mengeluarkan
+                    karya
+                    yang dimiliki yang nantinya akan berguna untuk diri sendiri dan orang lain.</p>
+                <h2>Benefitnya?</h2>
+                <div class="text-wrapper">
+                    <p class="list">-</p>
+                    <p> Kamu akan mendapatkan ilmu baru</p>
+                </div>
+                <div class="text-wrapper">
+                    <p class="list">-</p>
+                    <p> Meingkatkan motivasi kamu</p>
+                </div>
+                <div class="text-wrapper">
+                    <p class="list">-</p>
+                    <p> E-Sertifikat (jika kamu mendaftarkan akun di website ini)</p>
+                </div>
+                <div class="daftar-sem button">
+                    <a href="<?php echo site_url('profile/index').'?tab=e_semnas'; ?>" class="btn daftar-block">
+                        <span class="daftar-text">Daftar Sekarang</span>
+                    </a>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="logo-sem">
-        <img src="<?php echo base_url('public/pingfest/img/logosemnas.svg'); ?>" />
+        <div class="logo-sem">
+            <img src="<?php echo base_url('public/pingfest/img/logosemnas.svg'); ?>" />
 
-    </div>
-    <div class="informasi-sem">
-        <h1 class="title">Rincian Acara</h1>
-        <div class="box-wrapper">
-            <div class="box-section">
-                <div class="box-semnas">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 256 256">
-                        <path
-                            d="M128 4C59.5 4 4 59.5 4 128S59.5 252 128 252 252 196.5 252 128 196.5 4 128 4ZM236 128C236 187.35 187.95 236 128 236 68.65 236 20 187.95 20 128 20 68.65 68.05 20 128 20 187.35 20 236 68.05 236 128ZM161.55 172.15 120.95 142.65C119.4 141.5 118.5 139.7 118.5 137.8V58C118.5 54.7 121.2 52 124.5 52H131.5C134.8 52 137.5 54.7 137.5 58V131.15L172.75 156.8C175.45 158.75 176 162.5 174.05 165.2L169.95 170.85C168 173.5 164.25 174.1 161.55 172.15Z" />
-                    </svg>
-                    <h4>Waktu</h4>
-                    <div class="text-box-wrapper">
-                        <p>23 - 24 Oktober 2021</p>
-                        <p>18.00 WIB - Selesai</p>
+        </div>
+        <div class="informasi-sem">
+            <h1 class="title">Rincian Acara</h1>
+            <div class="box-wrapper">
+                <div class="box-section">
+                    <div class="box-semnas">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 256 256">
+                            <path
+                                d="M128 4C59.5 4 4 59.5 4 128S59.5 252 128 252 252 196.5 252 128 196.5 4 128 4ZM236 128C236 187.35 187.95 236 128 236 68.65 236 20 187.95 20 128 20 68.65 68.05 20 128 20 187.35 20 236 68.05 236 128ZM161.55 172.15 120.95 142.65C119.4 141.5 118.5 139.7 118.5 137.8V58C118.5 54.7 121.2 52 124.5 52H131.5C134.8 52 137.5 54.7 137.5 58V131.15L172.75 156.8C175.45 158.75 176 162.5 174.05 165.2L169.95 170.85C168 173.5 164.25 174.1 161.55 172.15Z" />
+                        </svg>
+                        <h4>Waktu</h4>
+                        <div class="text-box-wrapper">
+                            <p>23 - 24 Oktober 2021</p>
+                            <p>18.00 WIB - Selesai</p>
+                        </div>
+                    </div>
+                    <div class="box-semnas">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="55" height="50" viewBox="0 0 288 256">
+                            <path
+                                d="M279.438 76C278.438 76 277.438 76.2 276.438 76.6L191.438 108 186.288 106.2C197.938 88.75 207.438 70.6 207.438 57.5 207.438 23.5 178.788-4 143.438-4S79.438 23.55 79.438 57.5C79.438 63.4 81.438 70.4 84.638 77.8L9.487 104C3.438 106.4-.562 112.3-.562 118.85V244C-.562 248.6 3.188 252 7.438 252 8.438 252 9.438 251.8 10.438 251.4L95.438 220 181.438 250.35C187.938 252.5 194.938 252.55 201.438 250.45L277.388 224C283.488 221.55 287.438 215.7 287.438 209.15V84C287.438 79.4 283.688 76 279.438 76ZM87.438 205.9 15.388 232.5 14.737 119.05 87.438 93.8ZM143.438 12C169.888 12 191.438 32.4 191.438 57.5 191.438 71 172.388 101.95 143.438 135.9 114.488 101.95 95.438 71 95.438 57.5 95.438 32.4 116.988 12 143.438 12ZM183.438 234.1 103.438 205.85V110.4C115.638 128.05 129.488 144.4 137.288 153.25 138.888 155.1 141.188 156 143.438 156S147.938 155.1 149.588 153.25C155.988 146 166.438 133.7 176.738 119.8L183.438 122.15ZM199.438 234.2V122.1L271.488 95.5 272.138 208.95ZM155.438 60C155.438 53.35 150.038 48 143.438 48S131.438 53.35 131.438 60C131.438 66.6 136.838 72 143.438 72S155.438 66.65 155.438 60Z" />
+                        </svg>
+                        <h4>Tempat</h4>
+                        <div class="text-box-wrapper">
+
+                            <a href="https://www.youtube.com/channel/UCLUJ6DRel_x7jglM6iZB0lQ" target="_blank">
+                                <p>Youtube</p>
+                            </a>
+                            <a href="https://www.instagram.com/pingfest/" target="_blank">
+                                <p>Instagram</p>
+                            </a>
+                        </div>
                     </div>
                 </div>
-                <div class="box-semnas">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="55" height="50" viewBox="0 0 288 256">
-                        <path
-                            d="M279.438 76C278.438 76 277.438 76.2 276.438 76.6L191.438 108 186.288 106.2C197.938 88.75 207.438 70.6 207.438 57.5 207.438 23.5 178.788-4 143.438-4S79.438 23.55 79.438 57.5C79.438 63.4 81.438 70.4 84.638 77.8L9.487 104C3.438 106.4-.562 112.3-.562 118.85V244C-.562 248.6 3.188 252 7.438 252 8.438 252 9.438 251.8 10.438 251.4L95.438 220 181.438 250.35C187.938 252.5 194.938 252.55 201.438 250.45L277.388 224C283.488 221.55 287.438 215.7 287.438 209.15V84C287.438 79.4 283.688 76 279.438 76ZM87.438 205.9 15.388 232.5 14.737 119.05 87.438 93.8ZM143.438 12C169.888 12 191.438 32.4 191.438 57.5 191.438 71 172.388 101.95 143.438 135.9 114.488 101.95 95.438 71 95.438 57.5 95.438 32.4 116.988 12 143.438 12ZM183.438 234.1 103.438 205.85V110.4C115.638 128.05 129.488 144.4 137.288 153.25 138.888 155.1 141.188 156 143.438 156S147.938 155.1 149.588 153.25C155.988 146 166.438 133.7 176.738 119.8L183.438 122.15ZM199.438 234.2V122.1L271.488 95.5 272.138 208.95ZM155.438 60C155.438 53.35 150.038 48 143.438 48S131.438 53.35 131.438 60C131.438 66.6 136.838 72 143.438 72S155.438 66.65 155.438 60Z" />
-                    </svg>
-                    <h4>Tempat</h4>
-                    <div class="text-box-wrapper">
+                <div class="box-section">
+                    <div class="box-semnas">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 256 256">
+                            <path
+                                d="M248.971 112.971 143.03 7.03A24 24 0 0 0 126.059 0H24C10.745 0 0 10.745 0 24V126.059A24 24 0 0 0 7.03 143.029L112.971 248.971C122.343 258.343 137.539 258.344 146.912 248.971L248.971 146.912C258.343 137.539 258.343 122.343 248.971 112.97ZM237.657 135.598 135.598 237.657C132.477 240.779 127.411 240.784 124.285 237.657L18.343 131.716A7.948 7.948 0 0 1 16 126.059V24C16 19.589 19.589 16 24 16H126.059C128.196 16 130.205 16.832 131.716 18.343L237.657 124.284C240.776 127.404 240.776 132.479 237.657 135.598ZM72 62C77.514 62 82 66.486 82 72S77.514 82 72 82 62 77.514 62 72 66.486 62 72 62M72 48C58.745 48 48 58.745 48 72S58.745 96 72 96 96 85.255 96 72 85.255 48 72 48Z" />
+                        </svg>
+                        <h4>Harga</h4>
+                        <div class="text-box-wrapper">
 
-                        <a href="https://www.youtube.com/channel/UCLUJ6DRel_x7jglM6iZB0lQ" target="_blank">
-                            <p>Youtube</p>
-                        </a>
-                        <a href="https://www.instagram.com/pingfest/" target="_blank">
-                            <p>Instagram</p>
-                        </a>
+                            <p>GRATIS!</p>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="box-section">
-                <div class="box-semnas">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 256 256">
-                        <path
-                            d="M248.971 112.971 143.03 7.03A24 24 0 0 0 126.059 0H24C10.745 0 0 10.745 0 24V126.059A24 24 0 0 0 7.03 143.029L112.971 248.971C122.343 258.343 137.539 258.344 146.912 248.971L248.971 146.912C258.343 137.539 258.343 122.343 248.971 112.97ZM237.657 135.598 135.598 237.657C132.477 240.779 127.411 240.784 124.285 237.657L18.343 131.716A7.948 7.948 0 0 1 16 126.059V24C16 19.589 19.589 16 24 16H126.059C128.196 16 130.205 16.832 131.716 18.343L237.657 124.284C240.776 127.404 240.776 132.479 237.657 135.598ZM72 62C77.514 62 82 66.486 82 72S77.514 82 72 82 62 77.514 62 72 66.486 62 72 62M72 48C58.745 48 48 58.745 48 72S58.745 96 72 96 96 85.255 96 72 85.255 48 72 48Z" />
-                    </svg>
-                    <h4>Harga</h4>
-                    <div class="text-box-wrapper">
+                    <div class="box-semnas">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 256 256">
+                            <path
+                                d="M243.95 243.9 255.6 193.5C257.3 186.15 253.5 178.6 246.55 175.6L192.3 152.35C185.95 149.6 178.45 151.45 174.05 156.8L152 183.75C117.4 166.75 89.2 138.6 72.2 103.95L99.15 81.9C104.5 77.5 106.35 70 103.6 63.65L80.35 9.45C77.35 2.5 69.85-1.3 62.5.4L12.15 12C5 13.65 0 19.95 0 27.25 0 153.35 102.1 256 228.75 256 236.05 256 242.35 251 243.95 243.9ZM16 27.55 65.8 16.05 88.8 69.75 52.4 99.5C76.7 151.3 104.85 179.35 156.45 203.55L186.2 167.15 239.9 190.15 228.4 239.95C111.15 239.85 16.15 144.8 16 27.55Z" />
+                        </svg>
+                        <h4>Narahubung</h4>
+                        <div class="text-box-wrapper">
+                            <a href="http://wa.me/6281239930252" target="_blank">
+                                <p>(+62) 812 3993 0252 (sabil)</p>
+                            </a>
+                            <a href="http://wa.me/6282139893021" target="_blank">
+                                <p>(+62) 821 3989 3021 (lia)</p>
+                            </a>
+                        </div>
+                    </div>
 
-                        <p>GRATIS!</p>
-                    </div>
                 </div>
-                <div class="box-semnas">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 256 256">
-                        <path
-                            d="M243.95 243.9 255.6 193.5C257.3 186.15 253.5 178.6 246.55 175.6L192.3 152.35C185.95 149.6 178.45 151.45 174.05 156.8L152 183.75C117.4 166.75 89.2 138.6 72.2 103.95L99.15 81.9C104.5 77.5 106.35 70 103.6 63.65L80.35 9.45C77.35 2.5 69.85-1.3 62.5.4L12.15 12C5 13.65 0 19.95 0 27.25 0 153.35 102.1 256 228.75 256 236.05 256 242.35 251 243.95 243.9ZM16 27.55 65.8 16.05 88.8 69.75 52.4 99.5C76.7 151.3 104.85 179.35 156.45 203.55L186.2 167.15 239.9 190.15 228.4 239.95C111.15 239.85 16.15 144.8 16 27.55Z" />
-                    </svg>
-                    <h4>Narahubung</h4>
-                    <div class="text-box-wrapper">
-                        <a href="http://wa.me/6281239930252" target="_blank">
-                            <p>(+62) 812 3993 0252 (sabil)</p>
-                        </a>
-                        <a href="http://wa.me/6282139893021" target="_blank">
-                            <p>(+62) 821 3989 3021 (lia)</p>
-                        </a>
-                    </div>
-                </div>
-
             </div>
         </div>
-    </div>
-    <div class="pembicara-sem">
-        <h1 class="title">Pembicara</h1>
-        <div class="pembicara-wrapper">
-            <div class="pembicara">
-                <a href="https://www.youtube.com/c/WebProgrammingUNPAS" target="_blank">
-                    <img src="<?php echo base_url('public/pingfest/img/sandhika.png'); ?>" alt="sandhika">
-                </a>
-                <h2>Sandhika Galih</h2>
-                <p>Content Creator</p>
-                <p>Online Construtor</p>
-            </div>
-            <div class="pembicara">
-                <a href="https://www.youtube.com/c/KevinAnggara" target="_blank">
-                    <img src="<?php echo base_url('public/pingfest/img/kevin.png'); ?>" alt="kevin">
-                </a>
-                <h2>Kevin Anggara</h2>
-                <p>Penulis - Blogger</p>
-                <p>Content Creator</p>
+        <div class="pembicara-sem">
+            <h1 class="title">Pembicara</h1>
+            <div class="pembicara-wrapper">
+                <div class="pembicara">
+                    <a href="https://www.youtube.com/c/WebProgrammingUNPAS" target="_blank">
+                        <img src="<?php echo base_url('public/pingfest/img/sandhika.png'); ?>" alt="sandhika">
+                    </a>
+                    <h2>Sandhika Galih</h2>
+                    <p>Content Creator</p>
+                    <p>Online Instructor</p>
+                </div>
+                <div class="pembicara">
+                    <a href="https://www.youtube.com/c/KevinAnggara" target="_blank">
+                        <img src="<?php echo base_url('public/pingfest/img/kevin.png'); ?>" alt="kevin">
+                    </a>
+                    <h2>Kevin Anggara</h2>
+                    <p>Penulis - Blogger</p>
+                    <p>Content Creator</p>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="item sponsor">
-        <p>Our Sponsors</p>
-        <div class="sponsor-container">
-            <div class="big-wrapper">
-                <a class="sponsor-logo sponsor-big" href="https://barito.tech/" target="_blank"><img
-                        class="sponsor-image" src="<?php echo base_url('public/pingfest/img/sponsor/bit.png'); ?>"
-                        alt=""></a>
-                <a class="sponsor-logo sponsor-big" href="https://upscale.co.id/" target="_blank"><img
-                        class="sponsor-image" src="<?php echo base_url('public/pingfest/img/sponsor/upscale.png'); ?>"
-                        alt=""></a>
-            </div>
-            <div class="small-wrapper">
-                <div class="small-section">
-                    <a class="sponsor-logo sponsor-small" href="https://byu.id/" target="_blank"><img
-                            class="sponsor-image" src="<?php echo base_url('public/pingfest/img/sponsor/byu.png'); ?>"
+        <div class="item sponsor">
+            <p>Our Sponsors</p>
+            <div class="sponsor-container">
+                <div class="big-wrapper">
+                    <a class="sponsor-logo sponsor-big" href="https://barito.tech/" target="_blank"><img
+                            class="sponsor-image" src="<?php echo base_url('public/pingfest/img/sponsor/bit.png'); ?>"
                             alt=""></a>
-                    <a class="sponsor-logo sponsor-small" href=" https://dewaweb.com" target="_blank"><img
+                    <a class="sponsor-logo sponsor-big" href="https://upscale.co.id/" target="_blank"><img
                             class="sponsor-image"
-                            src="<?php echo base_url('public/pingfest/img/sponsor/dewaweb.png'); ?>" alt=""></a>
+                            src="<?php echo base_url('public/pingfest/img/sponsor/upscale.png'); ?>" alt=""></a>
                 </div>
-                <div class="small-section">
+                <div class="small-wrapper">
+                    <div class="small-section">
+                        <a class="sponsor-logo sponsor-small" href="https://byu.id/" target="_blank"><img
+                                class="sponsor-image"
+                                src="<?php echo base_url('public/pingfest/img/sponsor/byu.png'); ?>" alt=""></a>
+                        <a class="sponsor-logo sponsor-small" href=" https://dewaweb.com" target="_blank"><img
+                                class="sponsor-image"
+                                src="<?php echo base_url('public/pingfest/img/sponsor/dewaweb.png'); ?>" alt=""></a>
+                    </div>
+                    <div class="small-section">
 
-                    <a class="sponsor-logo sponsor-small" href="https://pahamify.com/" target="_blank"><img
-                            class="sponsor-image"
-                            src="<?php echo base_url('public/pingfest/img/sponsor/pahamify.png'); ?>" alt=""></a>
-                    <a class="sponsor-logo sponsor-small" href="https://www.instagram.com/whoknowswhat.id/"
-                        target="_blank"><img class="sponsor-image"
-                            src="<?php echo base_url('public/pingfest/img/sponsor/wkw.png'); ?>" alt=""></a>
+                        <a class="sponsor-logo sponsor-small" href="https://pahamify.com/" target="_blank"><img
+                                class="sponsor-image"
+                                src="<?php echo base_url('public/pingfest/img/sponsor/pahamify.png'); ?>" alt=""></a>
+                        <a class="sponsor-logo sponsor-small" href="https://www.instagram.com/whoknowswhat.id/"
+                            target="_blank"><img class="sponsor-image"
+                                src="<?php echo base_url('public/pingfest/img/sponsor/wkw.png'); ?>" alt=""></a>
+                    </div>
+
                 </div>
 
             </div>
-
         </div>
-    </div>
 
+    </div>
 </div>
 
 
